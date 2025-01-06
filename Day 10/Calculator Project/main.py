e922d11a5f533b2da680f4062cfba7b3a1c580de
Jack <jackmkj@gmail.com>
2025-01-06 21:23:47 +1100

    function with outputs, mutiple return values, docstrings

---
+++ Day 10/Calculator Project/main.py
@@ -0,0 +1,38 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+dic ={}
+dic["+"] = add
+dic["-"] = subtract
+dic["*"] = multiply
+dic["/"] = divide
+end = True
+while end:
+    import art
+    print(art.logo)
+    result = 0
+    n1 = float(input("what is the first number?: "))
+    continue_cal = True
+    while continue_cal:
+        print("+\n-\n*\n/\n")
+        operation = input("Pick an operation: ")
+        n2 = float(input("What is the next number: "))
+        result = (dic[operation](n1,n2))
+        print(n1,operation,n2,"=",result)
+        keep = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+        if keep == "y":
+            continue_cal = True
+            n1 = result
+        else:
+            continue_cal = False
+            end = True
+            print("\n"* 20)
---
+++ Day 10/Functions with Outputs/task.py
@@ -0,0 +1,7 @@
+f_name =input("what is your fname")
+l_name = input("what is your lname")
+
+def format_name(f_name,l_name):
+    print(f_name.title(),l_name.title())
+
+format_name(f_name=f_name,l_name=l_name)
