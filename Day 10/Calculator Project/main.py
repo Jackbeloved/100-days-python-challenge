610a2b1a061566b32561a8af664b55e53c0058a6
Jack <jackmkj@gmail.com>
2025-01-17 10:33:35 +1100

    tidy up code

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -15,15 +15,19 @@ dic["+"] = add
 dic["-"] = subtract
 dic["*"] = multiply
 dic["/"] = divide
+
+import art
+
 end = True
 while end:
-    import art
     print(art.logo)
     result = 0
     n1 = float(input("what is the first number?: "))
     continue_cal = True
     while continue_cal:
-        print("+\n-\n*\n/\n")
+        # print("+\n-\n*\n/\n")
+        for symbol in dic:
+            print(symbol)
         operation = input("Pick an operation: ")
         n2 = float(input("What is the next number: "))
         result = (dic[operation](n1,n2))
